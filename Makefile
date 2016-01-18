d098ebf33ffbd43986423943cdfbbce3fdfddc72
root <root@localhost.localdomain>
2016-01-18 15:56:45 -0800

    now we have to read from files
    reading from file

--- Makefile
+++ Makefile
@@ -1,13 +1,23 @@
 src = matrixMult.c
-prog = matrixMult
 
-all : $(prog)
+all : conventionnel conventionnelT
 
-$(prog) : $(src)
-	gcc -o $(prog) $(src) -I.
+conventionnel : $(src)
+	gcc -o conventionnel $(src) -I. -Dconventionnel 
+
+conventionnelT : $(src)
+	gcc -o conventionnelT $(src) -I. -DconventionnelT 
+
 clean   : 
-	rm -rf $(prog)
-run     : $(prog)
-	./$(prog)
+	rm -rf conventionnel conventionnelT
+
+regen   : Gen.csh
+	rm -rf ex_*
+	./Gen.csh
+
+cleanall:
+	make clean
+	rm -rf ex_*
 
 .PHONY : all clean run
+.SILENT : regen all
--- matrixMult.c
+++ matrixMult.c
@@ -1,30 +1,99 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 
+typedef struct matrices{
+	int *a;
+	int *b;
+	int *result;
+	int size;
+} Matrices;
 
-
-void  multiplyMatrix(int* matrixA,int* matrixB,int* result,int size){
+void  multiplyMatrix(Matrices *m){
+	double starting_time = clock();
 	int i = 0;
-	for(i ; i < size*size; i++){
+	for(i ; i < m->size*m->size; i++){
 		int j = 0;
-		for(j ; j <  size; j++){
-			result[i] += matrixA[j+(i/size)*size]*matrixB[j*size+i%size];
+		for(j ; j <  m->size; j++){
+			m->result[i] += m->a[j+(i/m->size)*m->size]*m->b[j*m->size+i%m->size];
 		} 
 	}
+	printf("the multiplication of two matrices of size %d took %lf s\n",m->size,(double)(clock()-starting_time)*(double)1000/CLOCKS_PER_SEC);
 }
 
-int main(){
-	int a[9] = {1,2,3,1,2,3,1,2,3};
-	int b[9] = {1,2,3,1,2,3,1,2,3};
-	int r[9] = {0,0,0,0,0,0,0,0,0};
-	int identity[9] = {1,0,0,0,1,0,0,0,1};
-	int size = 3;
-	multiplyMatrix(a,identity,r,size);
+void  multiplyMatrixT(Matrices *m){
+	double starting_time = clock();
 	int i = 0;
-	for (i = 0 ; i < size*size; i++){
-		printf("%d\t",r[i]);
-		if(i%size == size-1)
-			printf("\n");
+	for(i ; i < m->size*m->size; i++){
+		int j = 0;
+		for(j ; j <  m->size; j++){
+			m->result[i] += m->a[j+(i/m->size)*m->size]*m->b[j+(i%m->size)*m->size];
+		} 
 	}
+	printf("the multiplication of two matrices of size %d took %lf s\n",m->size,(double)(clock()-starting_time)*(double)1000/CLOCKS_PER_SEC);
+}
+
+int readFiles(char* file1,char* file2, Matrices *m){
+	FILE *f1,*f2;
+	f1 = fopen(file1,"r");
+	f2 = fopen(file2,"r");
+
+	fscanf(f1,"%d",&(m->size));
+	m->size = (1<<m->size);
+
+	m->a = malloc(m->size*m->size*sizeof(int));
+	m->b = malloc(m->size*m->size*sizeof(int));
+	m->result = malloc(m->size*m->size*sizeof(int));
+
+  int i = 0;
+	for(i; i < m->size*m->size ; i++){
+		fscanf(f1,"%d",&(m->a[i]));
+	}
+	
+	fscanf(f2,"%d",&(m->b[0]));
+ 
+	// Normal 
+#ifndef conventionnelT
+		for(i = 0 ; i < m->size*m->size ; i++){
+			fscanf(f2,"%d",&(m->b[i]));
+		}
+#else
+  // Transpose
+	  for(i = 0 ; i < m->size*m->size ; i++){
+		  fscanf(f2,"%d",&(m->b[i/m->size+m->size*(i%m->size)]));
+    }
+#endif
+	
+	fclose(f1);
+	fclose(f2);
+	return 0;
+}
+
+int main(int argc,char** argv){
+  if(argc < 3 ){
+		printf("this program takes at least 3 parameters\n"); 
+		printf("      -f <file1> <file2> [-p]\n");
+		return 1;
+	}
+  printf("%s %d\n",argv[3],argc);
+  Matrices m;
+	readFiles(argv[1],argv[2],&m);
+#ifdef conventionnel
+  multiplyMatrix(&m);
+#endif
+#ifdef conventionnelT
+	multiplyMatrixT(&m);
+#endif
+  if(argc >= 4){
+    if(strcmp(argv[3],"-p") == 0){
+      int i;	
+  	  for( i = 0 ; i < m.size*m.size; i++){
+		  	printf("%d\t",m.result[i]);
+		  	if(i%m.size == m.size - 1)
+			  	printf("\n");
+ 	    }
+    }
+  }
 	return 0;
 }
